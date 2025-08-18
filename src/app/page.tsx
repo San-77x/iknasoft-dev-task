e9461060b2dc1df4b1a02cc87b1bd77e7fd1696c
★ W O G G E R !!! <97348720+San-77x@users.noreply.github.com>
2025-08-18 23:39:02 +0530

    simple page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="min-h-screen">
+      <div className="container mx-auto px-5 max-w-6xl">
+        <h1 className="text-3xl font-bold text-gray-900 mb-8">
+          Simple Table Component
+        </h1>
+        <div className="overflow-x-auto">{/* Table */}</div>
+      </div>
+    </div>
+  );
 }
