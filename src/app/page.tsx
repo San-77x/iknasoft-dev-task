50033217a5ef92e7e119fc9bcfc894998cc04364
★ W O G G E R !!! <97348720+San-77x@users.noreply.github.com>
2025-08-19 00:02:40 +0530

    --

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,11 @@ import ReactTable from "./components/react-table";
 export default function Home() {
   return (
     <div className="min-h-screen">
-      <div className="container mx-auto px-5 max-w-6xl">
+      <div className="mx-auto px-5 max-w-6xl">
         <h1 className="text-3xl font-bold text-gray-900 mb-8">
           Simple Table Component
         </h1>
-        <div className="overflow-x-auto border">
-          <ReactTable />
-        </div>
+        <ReactTable />
       </div>
     </div>
   );
